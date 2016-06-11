d390e9dff077459223fbe91595a800de41cc17d9
Pedro-Souza <programadorhacker8@gmail.com>
2016-06-11 19:35:19 -0300

    Mais uma ferramenta tosca :3

---
+++ Pegip.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#_*_ coding: utf-8 _*_ 
+
+import os
+import sys
+import socket
+
+def main():
+	Sistema()
+	Resover()
+
+def Sistema():
+	if sys.platform in ['linux2','linux']:
+		os.system("clear")
+	else:
+		os.system("cls")
+
+def Resover():
+	if len(sys.argv) < 3:
+		print("Use python3 Pegip.py -h <host>")
+	else:
+		print(len(sys.argv))
+		host = sys.argv[2].replace("https://", "").replace("http://", "").replace("www", "")
+		print("IP: "+str(socket.gethostbyname(host)))
+
+main()
