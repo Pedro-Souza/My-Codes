5d31d57ba68bd0435e708b6f05c458b3203449d7
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-05 18:30:31 -0300

    bullshit

---
+++ ImPar.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import time
+
+ini = time.time()
+
+for i in range(0, 100):
+    if (i % 2) == 0:
+        print(str(i) + ' É par.')
+    else:
+        print(str(i) + ' É impar.')
+fim = time.time()
+print('Tempo de execução: ' + str(fim-ini))
