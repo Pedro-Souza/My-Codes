6a5e8cc0fe5a36f23a08fb972f5b9fc2bf439081
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-07 15:59:46 -0300

    updates

--- infinity.py
+++ infinity.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+from os import system
+from sys import platform
+
+
+def System():
+    if platform in ['linux', 'linux2']:
+        system('clear')
+    else:
+        system('cls')
+
+# In construction...
