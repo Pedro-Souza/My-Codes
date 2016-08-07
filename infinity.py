806bdf2a72d2d7e11ea86c8797498808bf533d2f
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-07 16:41:30 -0300

    main function

--- infinity.py
+++ infinity.py
@@ -3,6 +3,7 @@
 
 from os import system
 from sys import platform
+from itertools import permutations
 
 
 def System():
@@ -12,3 +13,19 @@ def System():
         system('cls')
 
 # In construction...
+
+
+def Infinity(word):
+    if "/" in word:
+        word = word.split("/")
+        words = ""
+        for i in word:
+            words += i
+    else:
+        words = word
+    arq = open("WordList.txt", "w", closefd=True)
+    for combinations in permutations(words):
+        arq.write("".join(combinations) + "\n")
+
+
+Infinity("Pedro")
