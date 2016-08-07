b1e26a9de4bcc8fea51489de3c0d2eca1d1ffacd
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-06 23:37:31 -0300

    auxilio trader

---
+++ ChameApi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import requests
+import json
+
+b = []
+req = json.loads(requests.get(
+    "https://chameleonbit.trade/api/v1/get_coin_data.php?id_coin=2").text)
+for i in req:
+    for a in req[i]:
+        b = a
+print(b['buy'])
