7dc8a56a5ecd5a1bad8646bd77e7c7b287b4f61c
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-10 17:55:39 -0300

    version 1.0

--- GitApi.py
+++ GitApi.py
@@ -36,7 +36,7 @@ class GitHub():
     def GetInfo(self, user):
         self.msg = ""
         req = loads(get('https://api.github.com/users/' + user).text)
-        self.msg += '\nInformation of user:'
+        self.msg += '\nInformation of user:\n'
         self.msg += '\nName: ' + str(req['name'])
         self.msg += '\nEmail: ' + str(req['email'])
         self.msg += '\nCompany: ' + str(req['company'])
@@ -47,27 +47,26 @@ class GitHub():
         self.msg += '\nFollowers: ' + str(req['followers']) + '\n'
         return self.msg
 
-
-def Arguments():
-    user = GitHub()
-    parser = ArgumentParser()
-    parser.add_argument('--repos', dest='repos', action='store_true',
-                        help='List all repository.')
-    parser.add_argument('--user', dest='user', action='store',
-                        required=True, help='Parameter for set user.')
-    parser.add_argument('--info', dest='info', action='store_true',
-                        help='Parameter for to get info of user')
-    parser.add_argument('--all', dest='all', action='store_true',
-                        help='Parameter for to define all options')
-    args = parser.parse_args()
-    if args.user and args.info:
-        print(user.GetInfo(args.user))
-    elif args.user and args.repos:
-        print(user.GetRepos(args.user))
-    elif args.user and args.all:
-        print(user.GetRepos(args.user))
-        print(user.GetInfo(args.user))
-    else:
-        print('Use --info, --repos or --all.')
-
-Arguments()
+    def Arguments(self):
+        self.user = GitHub()
+        self.parser = ArgumentParser()
+        self.parser.add_argument('--repos', dest='repos', action='store_true',
+                                 help='List all repository.')
+        self.parser.add_argument('--user', dest='user', action='store',
+                                 required=True, help='Parameter for set user.')
+        self.parser.add_argument('--info', dest='info', action='store_true',
+                                 help='Parameter for to get info of user')
+        self.parser.add_argument('--all', dest='all', action='store_true',
+                                 help='Parameter for to define all options')
+        self.args = self.parser.parse_args()
+        if self.args.user and self.args.info:
+            print(self.user.GetInfo(self.args.user))
+        elif self.args.user and self.args.repos:
+            print(self.user.GetRepos(self.args.user))
+        elif self.args.user and self.args.all:
+            print(self.user.GetRepos(self.args.user))
+            print(self.user.GetInfo(self.args.user))
+        else:
+            print('Use --info, --repos or --all.')
+a = GitHub()
+a.Arguments()
