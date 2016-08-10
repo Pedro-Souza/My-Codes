1f7f285be5a1d09f398d331ecd55d5eece57d218
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-10 08:06:10 -0300

    update

--- euler.py
+++ euler.py
@@ -1,22 +1,22 @@
-#_*_ coding:utf-8 _*_
-import os
-os.system("clear")
+# _*_ coding:utf-8 _*_
+from os import system
+system("clear")
 # Formula V-A+F=2
-print ("Formula: V-A+F=2 ")
+print("Formula: V-A+F=2 ")
 
 
 def valorF():
     # Faz o calculo para saber o valor de F
     B = (V - A) * -1
     F = B + 2
-    print ("O valor de F é: " + str(F) + ".")
+    print("O valor de F é: " + str(F) + ".")
 
 
 def valorV():
     # Faz o calculo para saber o valor de V
     B = (A + F) * -1
     V = B + 2
-    print ("O valor de V é: " + str(V) + ".")
+    print("O valor de V é: " + str(V) + ".")
     # V-A+F=2
 
 
@@ -24,7 +24,7 @@ def valorA():
     # Faz o calculo para saber o valor de A.
     B = (V + F) * -1
     A = B + 2
-    print ("O valor de A é: " + str(A) + ".")
+    print("O valor de A é: " + str(A) + ".")
 
 
 def op():
@@ -36,13 +36,13 @@ def op():
     elif res == 'A':
         valorA()
     else:
-        print ("Letra não encontrada.")
+        print("Letra não encontrada.")
 
 V = input("Valor de V: ")
 A = input("Valor de A: ")
 F = input("Valor de F: ")
 
 if V < 1 or A < 1 or F < 1:
-    print ("Poliedro Não existe.")
+    print("Poliedro Não existe.")
 else:
     op()
