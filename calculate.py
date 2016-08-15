0c1fef5e9f402fbf5d394bd48a34f16db7854165
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-15 12:00:31 -0300

    updates class

--- calculate.py
+++ calculate.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from math import sqrt, pi, tan
+from math import sqrt, pi
 
 
 class Calcular:
-    def __init__(self):
+    def __init__(self, n1, n2):
         self.description = "Shit of Calculator."
+        self.n1 = n1
+        self.n2 = n2
 
-    def Soma(self, n1, n2):
-        return n1 + n2
+    def Soma(self):
+        return self.n1 + self.n2
 
-    def Dividir(self, n1, n2):
-        return n1 / n2
+    def Dividir(self):
+        return self.n1 / self.n2
 
-    def Multiplicar(self, n1, n2):
-        return n1 * n2
+    def Multiplicar(self):
+        return self.n1 * self.n2
 
-    def Subtrair(self, n1, n2):
-        return n1 - n2
+    def Subtrair(self):
+        return self.n1 - self.n2
 
-    def Potencia(self, n1, n2):
-        return n1 ** n2
+    def Potencia(self):
+        return self.n1 ** self.n2
 
-    def Raiz(self, n):
-        return sqrt(n)
+    def Raiz(self):
+        return sqrt(self.n1)
 
     def Pi(self):
         return pi
 
-    def Tan(self, n1):
-        return tan(n1)
-
-calculator = Calcular()
-print(calculator.Raiz(9))
+calc = Calcular(10, 2)
+print(calc.Soma())
+print(calc.Dividir())
+print(calc.Multiplicar())
+print(calc.Subtrair())
+print(calc.Potencia())
+print(calc.Raiz())
+print(calc.Pi())
