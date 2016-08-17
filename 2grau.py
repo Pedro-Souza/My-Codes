4ddbf275d299b8ccb5543c87341f8fb50f77f70d
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-17 18:33:09 -0300

    updtade

--- 2grau.py
+++ 2grau.py
@@ -5,3 +5,8 @@
 class Calculater:
     def __init__(self):
         self.descri = "Simple library for calculation operation of 2 degree."
+
+    def delta_full(self, a, b, c):
+        return b**2 - 4 * a * c
+    
+    
