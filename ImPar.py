9aadd5a6f585a643c01e408f513f6fa205574ea7
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-20 11:20:07 -0300

    update time

--- ImPar.py
+++ ImPar.py
@@ -2,14 +2,14 @@
 # -*- coding:utf-8 -*-
 
 
-import time
+from time import time
 
-ini = time.time()
+ini = time()
 
 for i in range(0, 100):
     if (i % 2) == 0:
         print(str(i) + ' É par.')
     else:
         print(str(i) + ' É impar.')
-fim = time.time()
+fim = time()
 print('Tempo de execução: ' + str(fim - ini))
