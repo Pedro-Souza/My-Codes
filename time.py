0d5ef1c09c5d4b16351e6cb9e06511f41aeba8e5
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-22 19:55:03 -0300

    update

--- time.py
+++ time.py
@@ -1,6 +1,6 @@
-import time
-inicio = time.time()
-for i in range(1,10000):
+from time import time
+inicio = time()
+for i in range(1, 10000):
     print(i)
 fim = time.time()
-print(fim-inicio)
+print(fim - inicio)
