32ce6bd60f1fd7b9e4489a8ae657f4f5bb6e7753
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-22 20:04:37 -0300

    update

--- time.py
+++ time.py
@@ -1,6 +1,16 @@
 from time import time
-inicio = time()
-for i in range(1, 10000):
-    print(i)
-fim = time.time()
-print(fim - inicio)
+
+
+class Time:
+    def __init__(self):
+        self.descrii = "simple test of time."
+
+    def Test(self, dure=10000):
+        for i in range(dure):
+            print(i)
+
+init = time()
+ins = Time()
+ins.Test()
+fim = time()
+print(fim - init)
