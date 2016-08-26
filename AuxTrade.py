67926d0186815d6c02b201936f7a6a55fc4cfc64
Pedro-Souza <programadorhacker8@gmail.com>
2016-08-25 23:47:29 -0300

    add class

--- AuxTrade.py
+++ AuxTrade.py
@@ -4,6 +4,10 @@
 from requests import get
 from json import loads
 
+class AnaliseTrader:
+	def __init__(self):
+	    self.descri = "Script para auxiliar em analise de valores em Trader."
+    
 b = []
 req = loads(get(
     "https://chameleonbit.trade/api/v1/get_coin_data.php?id_coin=2").text)
