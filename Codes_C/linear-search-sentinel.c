26d185297c4b7fbb80d2b29409989342b4369970
Pedro-Souza <programadorhacker8@gmail.com>
2017-02-15 17:41:27 -0300

    added time

--- Codes_C/linear-search-sentinel.c
+++ Codes_C/linear-search-sentinel.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 int search(int vetor[], int n, int k){
     int i = 0;
@@ -15,19 +15,26 @@ int search(int vetor[], int n, int k){
 }
 
 int main(){
-    int vetor[4], n, k, resp;
-    printf("Digite valores inteiros:\n ");
-    for(int i = 0; i < 4; i++ ){
-        scanf("%d", &vetor[i]);
-    }
+    clock_t inicio, fim;
+    inicio = clock();
+    int n, k, resp;
+    int vetor[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+    //printf("Digite valores inteiros:\n ");
+    //for(int i = 0; i < 5; i++ ){
+    //    scanf("%d", &vetor[i]);
+    //}
+
     n = sizeof(vetor)/sizeof(int);
     printf("Qual valor quer buscar? ");
     scanf("%d", &k);
+    inicio = clock();
     resp = search(vetor, n, k);
     if(resp == 1){
         printf("Valor encontrado.");
     }else {
         printf("Achou não parsa.");
     }
+    fim = clock();
+    printf("Time: %f\n", (((fim-inicio) * 1000.00))/CLOCKS_PER_SEC);
     return 0;
 }
--- Codes_C/linear-search.c
+++ Codes_C/linear-search.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h>
 
 int search(int vetor[], int n, int k){
     for(int i = 0; i < n; i++){
@@ -12,21 +12,25 @@ int search(int vetor[], int n, int k){
 }
 
 int main(){
-
-    int vetor[5], n, k, resp;
-    printf("Entre com 5 valores inteiros: ");
-    for(int y = 0; y < 5; y++){
-        scanf("%d", &vetor[y]);
-    }
+    clock_t inicio, fim;
+    inicio = clock();
+    int n, k, resp;
+    int vetor[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+    //printf("Entre com 5 valores inteiros: ");
+    //for(int y = 0; y < 5; y++){
+    //    scanf("%d", &vetor[y]);
+    //}
 
     n = sizeof(vetor)/sizeof(int);
     printf("Qual valor deseja buscar? ");
     scanf("%d", &k);
 
     resp = search(vetor, n, k);
+    fim = clock();
     if(resp != -1){
         printf("O valor está no vetor.\n");
     }else {
         printf("O valor não está no vetor.\n");
     }
+    printf("Time: %f\n", (((fim-inicio) * 1000.0))/CLOCKS_PER_SEC);
 }
