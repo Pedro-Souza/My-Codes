4561805690966653af4961f5a940a2626fa80349
Pedro-Souza <programadorhacker8@gmail.com>
2017-02-23 11:21:30 -0300

    binary search with good code

--- Codes_C/binary_search.c
+++ Codes_C/binary_search.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
-int main(){
-    clock_t t_inicio, t_fim;
-    t_inicio = clock();
-    int vetor[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13};
-    int n, k, fim, inicio, aux, cont;
-    n = sizeof(vetor)/sizeof(int);
-    printf("Qual valor deseja buscar?: ");
-    scanf("%i", &k);
-    inicio, cont = 0, 0;
-    fim = n-1;
+void search(int vetor[], int inicio, int fim, int k){
+    int aux, cont;
+    cont = 0;
     aux = (inicio + fim) / 2;
-    while (inicio <= fim){
+    while(inicio <= fim){
         if(vetor[aux] < k){
             inicio = aux + 1;
-            cont += 1;
-        } else if(vetor[aux] == k) {
+            cont +=1;
+        } else if(vetor[aux] == k){
             printf("Found! Position: %d\n", aux+1);
             printf("Count: %d\n", cont);
             break;
@@ -27,10 +19,26 @@ int main(){
         }
         aux = (inicio + fim) / 2;
     }
-    if (inicio > fim){
+    if(inicio > fim){
         printf("Not found.\n");
         printf("Count: %d\n", cont);
     }
+}
+
+
+int main(){
+    int vetor[100], n;
+    n = sizeof(vetor)/sizeof(int);
+    for(int i = 0; i < n; i++ ){
+        vetor[i] = i;
+    }
+    clock_t t_inicio, t_fim;
+    t_inicio = clock();
+    int k, inicio, fim;
+    k = 99;
+    inicio = 0;
+    fim = n - 1;
+    search(vetor, inicio, fim, k);
     t_fim = clock();
     printf("Time: %f", (((t_fim - t_inicio) * 1000.0)) / CLOCKS_PER_SEC);
     return 0;
