9f749c8313793e219869b282d5e30dcf1dbea0d5
Pedro-Souza <programadorhacker8@gmail.com>
2017-03-15 18:52:03 -0300

    added function in bubble

--- Codes_C/bubble.c
+++ Codes_C/bubble.c
@@ -1,27 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int sort(int vetor[]){
-    printf("testes");
-    return 0;
-
-}
-
+void sort(int vet[], int tam);
 
 int main(){
     int vet[] = {9,10,7,1,2};
     int tam = sizeof(vet)/sizeof(int);
+    sort(vet, tam);
+    return 0;
+}
+
+void sort(int vet[], int tam){
     for(int i = 0; i < tam; i++){
         for(int j = 0; j < tam; j++){
-            if(vet[j] > vet[j + 1 ]){
+            if(vet[j] > vet[j+1]){
                 int aux = vet[j];
-                vet[j] = vet[j + 1];
-                vet[j + 1] = aux;
+                vet[j] = vet[j+1];
+                vet[j+1] = aux;
             }
         }
     }
-    for(int i = 0; i < tam; i++){
-        printf("%i - ", vet[i]);
-    }
-    return 0;
+    printf("Vetor ordenado.");
 }
