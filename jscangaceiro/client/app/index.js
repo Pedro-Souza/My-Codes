cb29b0e34b715d805ca17404131b37b9c955a198
Pedro-Souza <programadorhacker8@gmail.com>
2017-09-12 21:39:25 -0300

    atualização index e criando o .js

---
+++ jscangaceiro/client/app/index.js
@@ -0,0 +1,32 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'), 
+    document.querySelector('#quantidade')
+];
+
+console.log(campos);
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function(event){
+    event.preventDefault();
+    var tr = document.createElement('tr');
+    campos.forEach(function(campo) {
+        console.log(campo.value);
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+    var tdvolume = document.createElement('td');
+    tdvolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdvolume);
+    tbody.appendChild(tr);
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+    campos[0].focus();;
+
+});
+
+
+
