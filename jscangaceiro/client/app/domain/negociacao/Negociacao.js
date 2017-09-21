391c38ac6bdb724735ebdc1e59d76d88e6ffbc9a
Pedro-Souza <programadorhacker8@gmail.com>
2017-09-20 22:23:08 -0300

    add programação defensiva

--- jscangaceiro/client/app/domain/negociacao/Negociacao.js
+++ jscangaceiro/client/app/domain/negociacao/Negociacao.js
@@ -1,20 +1,19 @@
 class Negociacao{
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(_data, _quantidade, _valor){
+        Object.assign(this, {_quantidade, _valor});
+        this._data = new Date(_data.getTime());
+        Object.freeze(this);
+    } 
+    get volume() {
+        return this._quantidade * this._valor;
     }
-
+    get data() {
+        return new Date(this._data.getTime());
+    }
     get quantidade() {
         return this._quantidade;
     }
     get valor() {
-        return this._valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get volume() {
-        return this._quantidade * this._valor;
+        this._valor;
     }
 }
