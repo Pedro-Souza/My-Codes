9fed5b206ff5fa07e832189334df681e80f54202
Pedro Souza <programadorhacker8@gmail.com>
2017-12-17 21:58:19 -0200

    addd pomodoro

---
+++ Codes_JS/pomodoro/assets/js/app.js
@@ -0,0 +1,9 @@
+const timer = (time) => {
+    if(time == "25") {
+        console.log("fui chamado por 25");
+    } else if (time == "15") {
+        console.log('fui chamado no 15')
+    } else {  
+        console.log("agora estamos no elase")
+    }
+}
